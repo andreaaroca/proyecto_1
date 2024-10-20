52693285fbfdb75b4072bea972afeb0a076c9498
JuanDavidCalderon <jd.calderong12@uniandes.edu.co>
2024-10-19 20:39:53 -0500

    Estudiante

--- src/co/edu/andes/usuarios/Estudiante.java
+++ src/co/edu/andes/usuarios/Estudiante.java
@@ -2,16 +2,52 @@ package co.edu.andes.usuarios;
 
 import java.util.*;
 import LearningPath.LearningPath;
+import LearningPath.Resena;
 import Actividades.*;
+import java.time.*;
+
 
 public class Estudiante extends Usuario{
 	
 	private HashMap<Integer, LearningPath> lpInscritos;
+	private HashMap<Integer, Actividad> actividadesEnviadas;
+	private HashMap<Integer, Actividad> actividadesCompletadas;
+	
 
 	public Estudiante(String nombre, String nombreUsuario, String password, String tipoUsuario) {
         super(nombre, nombreUsuario, password, "Estudiante");
+        this.lpInscritos = new HashMap<Integer, LearningPath>();
     }
+
+	public HashMap<Integer, LearningPath> getLpInscritos() {
+		return lpInscritos;
+	}
 	  
+	//crear en lp una lista o mapa de instancias
+	public LocalDateTime iniciarLp(int idLp) {
+		
+	}
 	    
+	public LocalDateTime finalizarLp(int idLp) {
+	}
+
+	public LocalDateTime iniciarActividad() {
+		
+	}
+
+	public LocalDateTime enviarActividad() {
+		
+	}
+	
+	public void anadirMapaCalificadas(int idActividad,float nota) {
+		
+	}
+	
+	public void getActividadesEnviadas() {
+		
+	}
+	
+	
 	
 }
+	
